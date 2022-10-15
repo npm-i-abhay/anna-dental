b42b03c2b2bc45758c39afe39f396e45126049dc
Abhay <abakshi4@my.bcit.ca>
2022-10-15 10:06:19 -0700

    testing out the zooming glitch

--- comps/common/navigation/NavBar.js
+++ comps/common/navigation/NavBar.js
@@ -204,177 +204,85 @@ export const NavBar = () => {
   };
   return (
     <>
-      {sWidth < 600 ? (
-        <MobContainer>
-          <HamContainer
-            initial={false}
-            animate={isOpen ? "open" : "closed"}
-            // onClick={() => toggleOpen()}
-          >
-            <Background variants={sidebar}>
-              <HamBarContainer onClick={() => handleMenu()}>
-                <Bar1
-                  variants={barVariants}
-                  initial={"closed"}
-                  animate={isOpen ? "open" : "closed"}
-                />
-                <Bar2
-                  variants={barVariants}
-                  initial={"closedTwo"}
-                  animate={isOpen ? "openTwo" : "closedTwo"}
-                />
-                <Bar3
-                  variants={barVariants}
-                  initial={"closedThree"}
-                  animate={isOpen ? "openThree" : "closedThree"}
-                />
-              </HamBarContainer>
-              <Menu
-                display={menuDisplay}
-                variants={childrenVariants}
-                // animate={isOpen ? { backgroundColor: "white" } : "white"}
-              >
-                <LinkContainer variants={linkVariants}>
-                  <MenuLink href="/">Home</MenuLink>
-                </LinkContainer>
-
-                <LinkContainer variants={linkVariants}>
-                  <MenuLink onClick={() => handleMenu()} href="#contact">
-                    Contact
-                  </MenuLink>
-                </LinkContainer>
-
-                <LinkContainer variants={linkVariants}>
-                  <MenuLink
-                    variants={childrenVariants}
-                    onClick={() => toggleOpen()}
-                  >
-                    Services +
-                    <MobServicesContainer
-                      variants={servicesVariants}
-                      initial={false}
-                      animate={isServices ? "open" : "closed"}
-                    >
-                      {/* {serviceDropDown > 0 && ( */}
-                      <>
-                        <LinkContainer variants={linkVariants} passHref>
-                          <MenuLink href={"/service/solea"}>Solea</MenuLink>
-                        </LinkContainer>
-                        <LinkContainer variants={linkVariants} passHref>
-                          <MenuLink href={"/service/cerec"}> Cerec </MenuLink>
-                        </LinkContainer>
-                        <LinkContainer variants={linkVariants} passHref>
-                          <MenuLink href={"/service/new-office"}>
-                            New Office{" "}
-                          </MenuLink>
-                        </LinkContainer>
-                        <LinkContainer variants={linkVariants} passHref>
-                          <MenuLink href={"/service/sundries"}>
-                            {" "}
-                            Sundries
-                          </MenuLink>
-                        </LinkContainer>
-                      </>
-                      {/* )} */}
-                    </MobServicesContainer>
-                  </MenuLink>
-                </LinkContainer>
-
-                <LinkContainer
-                  onClick={() => handleMenu()}
-                  variants={linkVariants}
-                >
-                  {/* <MenuLink href="#reviews">Reviews</MenuLink> */}
-                </LinkContainer>
-              </Menu>
-            </Background>
-          </HamContainer>
-          <DesktopLogoContainer>
-            <a href={"/"} passHref>
-              <LogoImg src="/graphics/logo2.png" />
-            </a>
-          </DesktopLogoContainer>
-        </MobContainer>
-      ) : (
-        <Container
-          bgCol={theme.body}
-          width="100%"
-          // shadow={"2px 3px 5px  #999999;"}
-          radius="2px"
-          // filter={"blur(4px)"}
-          position="sticky"
-          top="0px"
-          height="80%"
-          direction="row"
-          zIndex={2}
-        >
-          <DesktopLogoContainer>
-            <Link href={"/"} passHref>
-              <LogoImg src="/graphics/logo2.png" />
-            </Link>
-          </DesktopLogoContainer>
-          <DesktopLinkContainer>
-            <Link href={"/"} passHref>
-              <MenuLink
-                initial={"rest"}
-                variants={LinkHover}
-                animate={"rest"}
-                whileHover={"hover"}
-              >
-                Home
-              </MenuLink>
-            </Link>
-
-            <div>
-              <MenuLink
-                href="#contact"
-                initial={"rest"}
-                variants={LinkHover}
-                animate={"rest"}
-                whileHover={"hover"}
-              >
-                Contact
-              </MenuLink>
-            </div>
-
-            <Link
-              href={"/"}
-              passHref
-              // style={{ background: "red", height: "100%" }}
-              // animate={serviceDropDown == 300 ? "open" : "closed"}
-              // variants={sidebar}
+      <Container
+        bgCol={theme.body}
+        width="100%"
+        // shadow={"2px 3px 5px  #999999;"}
+        radius="2px"
+        // filter={"blur(4px)"}
+        position="sticky"
+        top="0px"
+        height="80%"
+        direction="row"
+        zIndex={2}
+      >
+        <DesktopLogoContainer>
+          <Link href={"/"} passHref>
+            <LogoImg src="/graphics/logo2.png" />
+          </Link>
+        </DesktopLogoContainer>
+        <DesktopLinkContainer>
+          <Link href={"/"} passHref>
+            <MenuLink
+              initial={"rest"}
+              variants={LinkHover}
+              animate={"rest"}
+              whileHover={"hover"}
             >
-              <MenuLink
-                initial={"rest"}
-                variants={LinkHover}
-                animate={"rest"}
-                whileHover={"hover"}
-                onMouseEnter={() => setServiceDropDown(300)}
-                onMouseLeave={() => setServiceDropDown(0)}
-              >
-                Services
-                <ServicesContainer disp={serviceDropDown}>
-                  {serviceDropDown > 0 && (
-                    <>
-                      <Link href={"/service/solea"} passHref>
-                        <MenuLink>Solea</MenuLink>
-                      </Link>
-                      <Link href={"/service/cerec"} passHref>
-                        <MenuLink> Cerec </MenuLink>
-                      </Link>
-                      <Link href={"/service/new-office"} passHref>
-                        <MenuLink> New Office </MenuLink>
-                      </Link>
-                      <Link href={"/service/sundries"} passHref>
-                        <MenuLink> Sundries</MenuLink>
-                      </Link>
-                    </>
-                  )}
-                </ServicesContainer>
-              </MenuLink>
-            </Link>
+              Home
+            </MenuLink>
+          </Link>
 
-            {/* <div>
+          <div>
+            <MenuLink
+              href="#contact"
+              initial={"rest"}
+              variants={LinkHover}
+              animate={"rest"}
+              whileHover={"hover"}
+            >
+              Contact
+            </MenuLink>
+          </div>
+
+          <Link
+            href={"/"}
+            passHref
+            // style={{ background: "red", height: "100%" }}
+            // animate={serviceDropDown == 300 ? "open" : "closed"}
+            // variants={sidebar}
+          >
+            <MenuLink
+              initial={"rest"}
+              variants={LinkHover}
+              animate={"rest"}
+              whileHover={"hover"}
+              onMouseEnter={() => setServiceDropDown(300)}
+              onMouseLeave={() => setServiceDropDown(0)}
+            >
+              Services
+              <ServicesContainer disp={serviceDropDown}>
+                {serviceDropDown > 0 && (
+                  <>
+                    <Link href={"/service/solea"} passHref>
+                      <MenuLink>Solea</MenuLink>
+                    </Link>
+                    <Link href={"/service/cerec"} passHref>
+                      <MenuLink> Cerec </MenuLink>
+                    </Link>
+                    <Link href={"/service/new-office"} passHref>
+                      <MenuLink> New Office </MenuLink>
+                    </Link>
+                    <Link href={"/service/sundries"} passHref>
+                      <MenuLink> Sundries</MenuLink>
+                    </Link>
+                  </>
+                )}
+              </ServicesContainer>
+            </MenuLink>
+          </Link>
+
+          {/* <div>
               <MenuLink
                 initial={"rest"}
                 variants={LinkHover}
@@ -385,9 +293,8 @@ export const NavBar = () => {
                 Reviews
               </MenuLink>
             </div> */}
-          </DesktopLinkContainer>
-        </Container>
-      )}
+        </DesktopLinkContainer>
+      </Container>
     </>
   );
 };
@@ -405,7 +312,7 @@ const MobContainer = styled.div`
   width: 100%;
   margin-top: 0.25em;
   z-index: 999999;
-  backdrop-filter: blur(2px);
+  backdrop-filter: blur(px);
 `;
 const HamContainer = styled(motion.div)`
   padding: 0.4em;
